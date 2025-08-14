958267897fed6fa095192c5cd7814d2f6de10550
gustavo <gustavo@gmail.com>
2025-08-14 16:59:47 -0300

    Adicionei a parte de ver pedidos anteriores

--- pedido/urls.py
+++ pedido/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('olhar_produtos/', views.olhar_produtos, name='olhar_produtos'),
     path('adicionarNoCarrinho/<str:pk>/', views.adicionarNoCarrinho, name='adicionarNoCarrinho'),
-    #path('delete/<str:pk>/', views.deletar, name='deletar_prod'),
-    #path('editar_form/<str:pk>/', views.editar_form, name='editar_form_prod'),
+    path('delete/<str:pk>/', views.deletar, name='deletar_prod'),
+    path('diminuir/<str:pk>/', views.diminuir, name='diminuir_prod'),
+    path('aumentar/<str:pk>/', views.aumentar, name='aumentar_prod'),
+    path('verPedido/<str:pk>/', views.verPedido, name='verPedido'),
+    path('finalizar/', views.finalizar, name='finalizar'),
+    path('pedidos_anteriores/', views.pedidos_anteriores, name='pedidos_anteriores'),
     #path('editar/<str:pk>/', views.editar, name='editar_prod'),
 ]
--- pedido/views.py
+++ pedido/views.py
@@ -6,8 +6,8 @@ from .models import Pedido, ProdutoNoPedido
 # Create your views here.
 
 def index(request):
-    id_pedido = 1
-    produtos = ProdutoNoPedido.objects.filter(pedido__id=id_pedido)
+    pedido = Pedido.objects.order_by('-id').first()
+    produtos = ProdutoNoPedido.objects.filter(pedido=pedido)
     context = {'produtos':produtos,
                }
     return render(request, 'pedidos/index.html', context)
@@ -21,10 +21,13 @@ def olhar_produtos(request):
 
 
 def adicionarNoCarrinho(request,pk):
-    id_pedido = 1
+    pedido = Pedido.objects.order_by('-id').first()
     produto = Produto.objects.get(id=pk)
-    pedido = Pedido.objects.get(id=id_pedido)
-    prodNoPedido = ProdutoNoPedido.objects.get(produto=produto, pedido=pedido)
+    
+    try:
+        prodNoPedido = ProdutoNoPedido.objects.get(produto=produto, pedido=pedido)
+    except ProdutoNoPedido.DoesNotExist:
+        prodNoPedido = None
     if(prodNoPedido == None):
         prodNoPedido = ProdutoNoPedido(
             produto = produto,
@@ -36,3 +39,55 @@ def adicionarNoCarrinho(request,pk):
         prodNoPedido.quantidade = prodNoPedido.quantidade+1
         prodNoPedido.save()
     return redirect("/pedido/")
+
+
+def diminuir(request,pk):
+    id_produtoNoPedido = pk
+    prodNoPedido = ProdutoNoPedido.objects.get(id=id_produtoNoPedido)
+    prodNoPedido.quantidade = prodNoPedido.quantidade - 1
+    if prodNoPedido.quantidade == 0:
+        prodNoPedido.delete()
+    else:
+        prodNoPedido.save()
+    return redirect("/pedido/")
+
+
+def aumentar(request,pk):
+    id_produtoNoPedido = pk
+    prodNoPedido = ProdutoNoPedido.objects.get(id=id_produtoNoPedido)
+    prodNoPedido.quantidade = prodNoPedido.quantidade + 1
+    prodNoPedido.save()
+    return redirect("/pedido/")
+
+
+def deletar(request,pk):
+    id_produtoNoPedido = pk
+    prodNoPedido = ProdutoNoPedido.objects.get(id=id_produtoNoPedido)
+    prodNoPedido.delete()
+    return redirect("/pedido/")
+
+
+
+def finalizar(request):
+    pedido = Pedido.objects.order_by('-id').first()
+    pedido.status = 'solicitado'
+    pedido.save()
+    pedidoNovo = Pedido(
+        status = 'no_carrinho'
+    )
+    pedidoNovo.save()
+    return redirect("/pedido/")
+
+def pedidos_anteriores(request):
+    pedidos = Pedido.objects.order_by('-id')[1:]
+    context = {
+        'pedidos':pedidos,
+    }
+    return render(request, 'pedidos/pedidos_anteriores.html', context)
+
+def verPedido(request, pk):
+    pedido = Pedido.objects.get(id=pk)
+    produtos = ProdutoNoPedido.objects.filter(pedido=pedido)
+    context = {'produtos':produtos,
+               }
+    return render(request, 'pedidos/verPedido.html', context)
